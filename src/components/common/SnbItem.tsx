c0eb0afbf977cf68e8feff87a58805d4cdb52f25
하주영 <92720304+hazzuu123@users.noreply.github.com>
2024-06-23 14:15:54 +0000

    Refactor: SnbItem props 변경

--- src/components/common/SnbItem.tsx
+++ src/components/common/SnbItem.tsx
@@ -1,30 +1,33 @@
 import styled from "styled-components";
 import Title from "./Title";
 import { TSnbSchema } from "@/style/theme";
-import SnbText from "@/assets/images/SnbText.svg?react";
-interface Props {
-  children: string;
-  schema: Extract<TSnbSchema, "default" | "selected">;
-  image: TImage;
-  prevent: boolean;
-}
-type TImage = "text";
 
-const SnbItem = ({ children, schema, image, prevent }: Props) => {
+interface Props {
+  text: string;
+  schema: Extract<TSnbSchema, "default" | "selected">;
+  icon: React.ReactNode;
+  diasbled: boolean;
+}
+
+const SnbItem = ({ text, schema, icon, diasbled }: Props) => {
   return (
-    <SnbItemStyle schema={schema} image={image} prevent={prevent}>
-      <div className="icon"> {<SnbText />}</div>
+    <SnbItemStyle $schema={schema} $disabled={diasbled? "true": "false"}>
+      <div className="icon"> {icon}</div>
       <Title size="T6" color={schema === "default" ? "white" : "main"}>
-        {children}
+        {text}
       </Title>
     </SnbItemStyle>
   );
 };
 
-const SnbItemStyle = styled.div<Omit<Props, "children">>`
+interface SnbItemStyleProps {
+  $schema: string;
+  $disabled: string;
+}
+const SnbItemStyle = styled.div<SnbItemStyleProps>`
   &:hover{
-    background: ${({ prevent, theme }) => (prevent ? "" : theme.snbSchema["hover"].background)};
-    color: ${({ prevent, theme }) => (prevent ? "" : theme.snbSchema["hover"].color)};
+    background: ${({ $disabled, theme }) => ($disabled ==="true" ? "" : theme.snbSchema["hover"].background)};
+    color: ${({ $disabled, theme }) => ($disabled === "true" ? "" : theme.snbSchema["hover"].color)};
   }
   display: flex;
   width: 280px;
@@ -33,11 +36,13 @@ const SnbItemStyle = styled.div<Omit<Props, "children">>`
   align-items: center;
   gap: 16px;
   border-radius: 12px;
-  background: ${({ theme, schema }) => theme.snbSchema[schema].background};
-  color: ${({ theme, schema }) => theme.snbSchema[schema].color};
+  background: ${({ theme, $schema }) => theme.snbSchema[$schema].background};
+  color: ${({ theme, $schema }) => theme.snbSchema[$schema].color};
   .icon {
+    display: flex;
+    align-items: center;
     svg {
-      color: ${({ theme, schema }) => theme.snbSchema[schema].color};
+      color: ${({ theme, $schema }) => theme.snbSchema[$schema].color};
     }
 `;
 export default SnbItem;
