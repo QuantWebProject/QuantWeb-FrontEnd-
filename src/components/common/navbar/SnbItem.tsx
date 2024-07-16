0b01d2a07bcbe89ff6f16ae5726a2957848ca15a
mysoo <80390638+mysoo@users.noreply.github.com>
2024-07-16 22:26:24 +0900

    Fix: Navbar Link

--- src/components/common/navbar/SnbItem.tsx
+++ src/components/common/navbar/SnbItem.tsx
@@ -1,22 +1,29 @@
 import styled from "styled-components";
 import Title from "../Title";
 import { TSnbSchema } from "@/style/theme";
-
+import { NavLink } from "react-router-dom";
 interface Props {
   text: string;
   schema: Extract<TSnbSchema, "default" | "selected">;
   icon: React.ReactNode;
   diasbled: boolean;
+  path: string;
 }
 
-const SnbItem = ({ text, schema, icon, diasbled }: Props) => {
+const SnbItem = ({ text, schema, icon, diasbled, path }: Props) => {
   return (
-    <SnbItemStyle $schema={schema} $disabled={diasbled ? "true" : "false"}>
-      <div className="icon"> {icon}</div>
-      <Title size="T6" color={schema === "default" ? "white" : "main"}>
-        {text}
-      </Title>
-    </SnbItemStyle>
+    <NavStyle
+      to={path}
+      $schema={schema}
+      $disabled={diasbled ? "true" : "false"}
+    >
+      <SnbItemStyle $schema={schema} $disabled={diasbled ? "true" : "false"}>
+        <div className="icon"> {icon}</div>
+        <Title size="T6" color={schema === "default" ? "white" : "main"}>
+          {text}
+        </Title>
+      </SnbItemStyle>
+    </NavStyle>
   );
 };
 
@@ -24,13 +31,36 @@ interface SnbItemStyleProps {
   $schema: string;
   $disabled: string;
 }
+//TODO: NavStyle, SnItemStyle 병합 및 스타일 정리 필요. navbar text color 반영 필요
+const NavStyle = styled(NavLink)<SnbItemStyleProps>`
+  text-decoration: none;
+  border-radius: 12px;
+
+  &.active {
+    .icon {
+      filter: brightness(0) saturate(100%) invert(59%) sepia(75%)
+        saturate(5825%) hue-rotate(226deg) brightness(99%) contrast(106%);
+    }
+
+    background: ${({ $disabled, theme }) =>
+      $disabled === "true" ? "" : theme.snbSchema["selected"].background};
+    color: ${({ $disabled, theme }) =>
+      $disabled === "true" ? "" : theme.snbSchema["selected"].color};
+  }
+`;
+
 const SnbItemStyle = styled.div<SnbItemStyleProps>`
   &:hover {
+    .icon {
+      filter: brightness(0) saturate(100%) invert(59%) sepia(75%)
+        saturate(5825%) hue-rotate(226deg) brightness(99%) contrast(106%);
+    }
     background: ${({ $disabled, theme }) =>
       $disabled === "true" ? "" : theme.snbSchema["hover"].background};
     color: ${({ $disabled, theme }) =>
       $disabled === "true" ? "" : theme.snbSchema["hover"].color};
   }
+
   display: flex;
   width: 280px;
   height: 64px;
@@ -44,6 +74,7 @@ const SnbItemStyle = styled.div<SnbItemStyleProps>`
   .icon {
     display: flex;
     align-items: center;
+
     svg {
       color: ${({ theme, $schema }) => theme.snbSchema[$schema].color};
     }
