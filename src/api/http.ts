482106930143de187c28f1190190bc10586056a3
하주영 <ju980304@naver.com>
2024-08-05 20:59:46 +0900

    Feat: axiosInstance 작성

---
+++ src/api/http.ts
@@ -0,0 +1,39 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+
+const BASE_URL =
+  "http://ec2-43-203-37-134.ap-northeast-2.compute.amazonaws.com:8080";
+const TIME_OUT = 30000;
+
+export const http = axios.create({
+  baseURL: BASE_URL,
+  timeout: TIME_OUT
+});
+
+const onResponse = (res: AxiosResponse) => {
+  return res;
+};
+
+const onError = (error: AxiosError | Error) => {
+  if (error instanceof AxiosError) {
+    if (!error.response) {
+      console.log(`❌ Network Error`);
+    } else {
+      const { status } = error.response;
+      if (status >= 500) {
+        console.log(`❌ Server Error`);
+      } else if (status === 400) {
+        console.log(`❌ InValid Request Error`);
+      } else if (status === 401) {
+        console.log(`❌ InValid Token Error`);
+      } else {
+        console.log(`❌ ${status} Error`);
+      }
+    }
+  } else {
+    console.log(`❌ ${error.message}`);
+  }
+
+  return Promise.reject(error);
+};
+
+http.interceptors.response.use(onResponse, onError);
