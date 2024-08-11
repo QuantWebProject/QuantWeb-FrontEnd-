dbcb42b3018cd9f72a52d85b019a7f0ce3a98771
하주영 <ju980304@naver.com>
2024-08-11 17:53:39 +0900

    Feat: /api로 시작하는 모든 요청에 대해 프록시를 붙임

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        secure: false,
+        ws: true
+      }
+    }
+  },
   plugins: [
     react(),
     tsconfigPaths(),
