4ba8c6f9424571d99578de62193326cafad244bd
하주영 <ju980304@naver.com>
2024-08-11 17:52:36 +0900

    Feat: 모달 관련 기능 훅으로 분리

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,43 +1,20 @@
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 
 const useModal = (open: boolean) => {
-  const modalRef = useRef<HTMLDivElement | null>(null);
   const [isOpen, setIsOpen] = useState(open);
 
-  // 모달창 닫기
-  const handleModalClose = () => {
+  const modalOpen = () => {
+    setIsOpen(true);
+  };
+
+  const modalClose = () => {
     setIsOpen(false);
   };
 
-  // 모달창 바깥을 누르면 닫힘
-  const handleOverlayClick = (e: React.MouseEvent) => {
-    if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
-      setIsOpen(false);
-    }
-  };
-
-  // esc 키를 누르면 닫힘
-  useEffect(() => {
-    const handleKeyDown = (e: KeyboardEvent) => {
-      if (e.key === "Escape") {
-        setIsOpen(false);
-      }
-    };
-
-    if (isOpen) {
-      window.addEventListener("keydown", handleKeyDown);
-    } else {
-      window.removeEventListener("keydown", handleKeyDown);
-    }
-
-    return () => window.removeEventListener("keydown", handleKeyDown);
-  }, [isOpen]);
-
   return {
-    modalRef,
     isOpen,
-    handleModalClose,
-    handleOverlayClick
+    modalOpen,
+    modalClose
   };
 };
 
